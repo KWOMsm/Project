ff88642b036ee6bafeb95dfeff05cc817bddc3ac
KWOMsm <rnjstjdals74@gmail.com>
2020-12-09 16:23:18 +0900

    git push

--- moneyBook.c
+++ moneyBook.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ncurses.h>
+//#include <ncurses.h>
 
 #define moneyTRUE 1
 #define moneyFALSE 0
@@ -14,14 +14,9 @@ static char plan_date[BUFSIZ];
 static char  plan_money[BUFSIZ];
 static char *a[3];
 
-static int sum = 0;
-static char money[BUFSIZ];
-static char plan_date[BUFSIZ];
-static char  plan_money[BUFSIZ];
-static char *a[3];
-
 
 int option() {
+
     int opt;
 
     printf("===== input option =====\n");
@@ -34,7 +29,9 @@ int option() {
 
     scanf("%d", &opt);
     return opt;
+    
 }
+
 
 void makePlan() {
     FILE *wplan;
@@ -89,8 +86,10 @@ void print_total_spend() {
     char str[100];
 
     if((rfp = fopen("moneyWrite.txt", "r")) == NULL) {
-        perror("fopen");
-        exit(1);
+        printf("===== ===== ===== ===== ===== =====\n");
+        printf("There is no expenditure history.\n");
+        printf("===== ===== ===== ===== ===== =====\n");
+        return;
     }
 
     while(fgets(str, 100, rfp) != NULL) {
@@ -102,7 +101,7 @@ void print_total_spend() {
 
 void advice() {
     FILE *fp;
-    int total;
+    int total = 0;
     int send=0;
     char buf[BUFSIZ];
     int res;
@@ -131,9 +130,10 @@ void advice() {
 }
 
 void moneyWrite() {
+    // ex)
     // 11/19 entertain 1000
     // 11/20 tax 5000
-    // moneyBook
+
     FILE *wfp, *rCategory;
     int c;
 
@@ -158,6 +158,7 @@ void moneyWrite() {
     while(fgets(categoryRead, 100, rCategory) != NULL) {
         printf("%s", categoryRead);
     }
+
     printf("\n===== ======== =====\n");
 
     printf("input your money spend\n");
@@ -195,6 +196,7 @@ void moneyRead() {
     printf("===== READ OPTION =====\n");
     printf("1. TOTAL SPEND\n");
     printf("2. ADVICE\n");
+    printf("OPTION : ");
 
     scanf("%d", &opt);
     switch(opt) {
@@ -326,112 +328,6 @@ void categorySetting() {
 }
 
 int main(void) {
-    // ncurses
-    int x = 0, y = 0, i = 0;
-	int max_x = 0, max_y = 0;
-	int next_x = 0;
-	int direction = 1;
-	int ch = 0; // init
-	int msg = 0; // init
-	char buffer[8]={0,};
-	WINDOW *mywin = NULL;
-
-    initscr(); // start curses mode
-	cbreak(); // line buffering disabled; pass on everything to me
-
-	if( has_colors() == FALSE ) { // color is not supported
-		endwin();
-		printf("Your terminal does not support color\n");
-		exit(1);
-	}
-	start_color(); // start the color mode
-	/*
-	 * define a new color-pair to use it later
-	 * : init_pair(index, foreground, background)
-	 */
-	init_pair(KWON_COLOR, COLOR_GREEN, COLOR_BLACK);
-	noecho();
-	keypad(stdscr, TRUE); // call after initscr, to understand KEY strokes
-	curs_set(FALSE);
-
-	getmaxyx(stdscr, max_y, max_x);
-	x = max_x / 2;
-	y = max_y / 2;
-
-    int nItem = 5;
-	char menus[5][30]={{"  write"}, {"  read"}, {"  plan"}, {"  category setting"}, {"  QUIT"}};
-	char selMenus[5][30]={{"> write"}, {"> read"}, {"> plan"}, {"> category setting"}, {"> QUIT"}};
-	char description[5][64]={{"Info: Write your expenditure"},{"Info: Check your expenditure"},{"Info: Write your plan!"}, {"Info: Set category"}, {"Info: QUIT PROGRAM"}};
-	int currItem = 0;
-	int cont = moneyTRUE;
-	int nRemainingItems = nItem;
-	// int deleted[5]={0,0,0,0,0};
-	// char *deletedMenu="  (deleted)";
-	// char *deletedMenuSel="> (deleted)";
-	
-	int boxWidth = 20;
-	int boxHeight = 20;
-	int boxY = 20;
-	int boxX = 0;
-
-    while( cont ) { 
-		clear(); // clearn the screen, and place the cursor in upper-left corner
-		// write characters on the buffer, and print out when refresh is called
-		for( i = 0 ; i < nItem ; i++ ) {
-			if( currItem == i ) {
-				attron(COLOR_PAIR(KWON_COLOR));
-				mvprintw(i,0,selMenus[i]);
-				attroff(COLOR_PAIR(KWON_COLOR));
-			} else {
-                mvprintw(i,0,menus[i]);
-            }
-		}
-		mvhline(i, 0, '_', max_x/2); // draw a horizontal line
-		mvprintw(++i,0,description[currItem]); // prints out a message
-		
-		refresh();
-
-		ch = (int) getch();	
-
-		switch( ch ) {
-			case KEY_UP:
-				if( currItem > 0 ) currItem -= 1;
-				break;
-        	case KEY_DOWN:
-				if( currItem + 1 < nItem ) currItem += 1;
-				break;
-            case 10:  
-                switch(currItem) {
-                    case 1:
-                        moneyWrite();
-                        break;
-                    case 2:
-                        moneyRead();
-                        break;
-                    case 3:
-                        moneyPlan();
-                        break;
-                    case 4:
-                        categorySetting();
-                        break;
-                    case 5:
-                        cont = moneyFALSE;
-                }
-                break;
-			case 'q':
-				cont = moneyFALSE;
-				break;
-			default:
-				break;
-		}
-	}
-	endwin();
-
-
-
-
-    // normal
-
     int opt;
 
     while(1) {
